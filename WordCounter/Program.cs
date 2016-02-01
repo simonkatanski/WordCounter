21e6157d49ce78206ab6d3b55a6e003553485e7c
simonkatanski <simon.katanski@gmail.com>
2016-02-01 01:29:12 +0100

    Added basic exception handling

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
 using WordCounter.Data;
 using WordCounter.Services;
 using WordCounter.Shared.Interfaces;
@@ -9,19 +11,48 @@ namespace WordCounter
     {
         static void Main(string[] args)
         {
-            IAppConfiguration configuration = new AppConfiguration();
             IAssemblyTypeLoader assemblyTypeLoader = new AssemblyTypeLoader();
-            var inputDataProvider = assemblyTypeLoader.LoadType<IInputDataProvider>(configuration.ProviderFactoryAssemblyPath);
-            var dataPersistor = assemblyTypeLoader.LoadType<IOutputDataPersistor>(configuration.ProviderFactoryAssemblyPath);
+            IAppConfiguration configuration = new AppConfiguration();
 
+            try
+            {                
+                var inputDataProvider = assemblyTypeLoader.LoadType<IInputDataProvider>(configuration.ProviderFactoryAssemblyPath);
+                var dataPersistor = assemblyTypeLoader.LoadType<IOutputDataPersistor>(configuration.ProviderFactoryAssemblyPath);
+                LaunchSentenceProcessing(inputDataProvider, dataPersistor);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                Console.WriteLine("An error occurred during the configuration load. Please check AppConfig is correctly set up. Error details: {0}", ex.Message);
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("An error occurred. Error details: {0}", ex.Message);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("An error occurred. Provided path is incorrect. Error details: {0}", ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error occurred during the assembly load. Please check if, dependencies are correctly set up. Error details: {0}", ex.Message);
+            }
+            catch (TypeLoadException ex)
+            {
+                Console.WriteLine("An error occurred during the assembly load. Error details: {0}", ex.Message);
+            }
+
+            Console.WriteLine("Application has finished running and will be closed after key press.");
+            Console.ReadKey();
+        }
+
+        static void LaunchSentenceProcessing(IInputDataProvider inputDataProvider, IOutputDataPersistor dataPersistor)
+        {
             using (var reader = inputDataProvider.GetInputDataReader())
             {
                 ISentenceProcessor sentenceProcessor = new SentenceProcessor(new char[] { ' ', ',', '.', ';', ':', '\t' });
                 var dictionary = sentenceProcessor.Process(reader);
                 dataPersistor.PersistData(dictionary);
             }
-
-            Console.ReadKey();
         }
     }
 }
